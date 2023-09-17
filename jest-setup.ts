d213a38f3cc86dc66fe273a23d88766d1313013f
Ben Durrant <ben.j.durrant@gmail.com>
2023-09-17 12:23:40 +0100

    minify build

--- jest-setup.ts
+++ jest-setup.ts
@@ -1,15 +1,13 @@
+/* eslint-disable @typescript-eslint/no-empty-interface */
 import * as mixNMatchers from "./src";
 
 declare global {
   // eslint-disable-next-line @typescript-eslint/no-namespace
   namespace jest {
-    // eslint-disable-next-line @typescript-eslint/no-empty-interface
     interface Matchers<R> extends mixNMatchers.MixNMatchers<R> {}
 
-    // eslint-disable-next-line @typescript-eslint/no-empty-interface
     interface Expect extends mixNMatchers.AsymmetricMixNMatchers {}
 
-    // eslint-disable-next-line @typescript-eslint/no-empty-interface
     interface InverseAsymmetricMatchers
       extends mixNMatchers.AsymmetricMixNMatchers {}
   }
