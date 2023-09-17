c10259a66d7b51d79e6b88e3b874097fea5e5447
Ben Durrant <ben.j.durrant@gmail.com>
2023-09-17 23:21:49 +0100

    use all entry point for own jest setup

--- jest-setup.ts
+++ jest-setup.ts
@@ -1,16 +1,1 @@
-/* eslint-disable @typescript-eslint/no-empty-interface */
-import * as mixNMatchers from "./src";
-
-declare global {
-  // eslint-disable-next-line @typescript-eslint/no-namespace
-  namespace jest {
-    interface Matchers<R> extends mixNMatchers.MixNMatchers<R> {}
-
-    interface Expect extends mixNMatchers.AsymmetricMixNMatchers {}
-
-    interface InverseAsymmetricMatchers
-      extends mixNMatchers.AsymmetricMixNMatchers {}
-  }
-}
-
-expect.extend(mixNMatchers);
+import "./src/all";
