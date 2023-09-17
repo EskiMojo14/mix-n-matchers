3d9832c625c783c4bbeb5c9765afae0a57935b04
Ben Durrant <ben.j.durrant@gmail.com>
2023-09-17 11:48:31 +0100

    move asymmetric matchers to separate folder

---
+++ src/asymmetricMatchers/index.ts
@@ -0,0 +1,6 @@
+// eslint-disable-next-line @typescript-eslint/no-empty-interface
+export interface AsymmetricMixNMatchers {}
+// eslint-disable-next-line @typescript-eslint/no-empty-interface
+export interface InverseAsymmetricMixNMatchers {}
+
+export { exactly } from "./exactly";
