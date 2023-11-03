161a3846dcdffee7dda4aa0b0d73a04217cae1c0
ben.durrant <ben.durrant@actual-experience.com>
2023-11-03 11:08:08 +0000

    clean up lint disable

--- src/jest-globals.ts
+++ src/jest-globals.ts
@@ -3,9 +3,7 @@ import { expect } from "@jest/globals";
 import * as mixNMatchers from "./index";
 
 declare module "@jest/expect" {
-  // eslint-disable-next-line @typescript-eslint/no-empty-interface
   interface Matchers<R> extends mixNMatchers.MixNMatchers<R> {}
-  // eslint-disable-next-line @typescript-eslint/no-empty-interface
   interface AsymmetricMatchers extends mixNMatchers.AsymmetricMixNMatchers {}
 }
 
