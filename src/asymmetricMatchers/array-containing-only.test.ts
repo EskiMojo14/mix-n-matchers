05f99cf47153a5b174668bdcb090f56ddea14163
EskiMojo14 <ben.j.durrant@gmail.com>
2024-02-18 19:33:48 +0000

    add more JSDoc

--- src/asymmetricMatchers/array-containing-only.test.ts
+++ src/asymmetricMatchers/array-containing-only.test.ts
@@ -5,9 +5,13 @@ expect.addSnapshotSerializer(alignedAnsiStyleSerializer);
 describe("arrayContainingOnly", () => {
   it("should expect an array to contain only the expected values", () => {
     expect([1, 2, 2, 3]).toEqual(expect.arrayContainingOnly([1, 2, 3]));
+
     expect(() => {
       expect([1, 2, 2, 3]).toEqual(expect.arrayContainingOnly([1, 2]));
     }).toThrowErrorMatchingSnapshot();
+
+    // vs
+    expect([1, 2, 2, 3]).toEqual(expect.arrayContaining([1, 2]));
   });
   it("should throw an error if the expected value is not an array", () => {
     expect(() => {
--- src/asymmetricMatchers/array-containing-only.ts
+++ src/asymmetricMatchers/array-containing-only.ts
@@ -2,6 +2,17 @@ import { printExpected, printReceived } from "jest-matcher-utils";
 import { makeEqualValue } from "../utils";
 import type { MatcherFunction } from "../utils/types";
 
+/**
+ * Matches any array that contains only the expected values.
+ * The array may contain as many of each expected value as it likes (or none), but no other values.
+ *
+ * @example
+ * expect([1, 2, 2, 3]).toEqual(expect.arrayContainingOnly([1, 2, 3])); // pass
+ * expect([1, 2, 2, 3]).toEqual(expect.arrayContainingOnly([1, 2])); // fail
+ *
+ * // whereas
+ * expect([1, 2, 2, 3]).toEqual(expect.arrayContaining([1, 2])); // pass
+ */
 export const arrayContainingOnly: MatcherFunction<[expected: Array<unknown>]> =
   function (received, expected) {
     if (!Array.isArray(expected)) {
@@ -30,11 +41,14 @@ declare module "./index" {
      * Use `arrayContainingOnly` to expect an array to contain only the expected values.
      * The array may contain as many of each expected value as it likes (or none), but no other values.
      *
+     * Optionally, you can provide a type for the elements using a generic.
      * @example
-     *
      * expect([1, 2, 2, 3]).toEqual(expect.arrayContainingOnly([1, 2, 3])); // pass
      * expect([1, 2, 2, 3]).toEqual(expect.arrayContainingOnly([1, 2])); // fail
+     *
+     * // whereas
+     * expect([1, 2, 2, 3]).toEqual(expect.arrayContaining([1, 2])); // pass
      */
-    arrayContainingOnly(expected: Array<unknown>): boolean;
+    arrayContainingOnly<E = any>(expected: Array<E>): boolean;
   }
 }
