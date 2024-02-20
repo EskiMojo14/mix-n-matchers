b77b990ebc4612d2389cac9472eee6c88959dc2d
Ben Durrant <ben.durrant@marketdojo.com>
2024-02-20 17:40:03 +0000

    add not tests for oneOf

--- src/asymmetricMatchers/one-of.test.ts
+++ src/asymmetricMatchers/one-of.test.ts
@@ -7,10 +7,14 @@ describe("oneOf", () => {
     expect(1).toEqual(expect.oneOf([1, 2, 3]));
     expect(2).toEqual(expect.oneOf([1, 2, 3]));
     expect(3).toEqual(expect.oneOf([1, 2, 3]));
+    expect(4).toEqual(expect.not.oneOf([1, 2, 3]));
   });
   it("should fail if value does not match any of the provided values", () => {
     expect(() => {
       expect(4).toEqual(expect.oneOf([1, 2, 3]));
+    }).toThrowErrorMatchingSnapshot();
+    expect(() => {
+      expect(1).toEqual(expect.not.oneOf([1, 2, 3]));
     }).toThrowErrorMatchingSnapshot();
   });
 });
