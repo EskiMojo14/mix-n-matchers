c22fd136f44e538251d202a2e5d0a80f2ef3150a
EskiMojo14 <ben.j.durrant@gmail.com>
2024-05-16 01:18:17 +0100

    nick TestFile component from jest-extended

---
+++ website/src/components/TestFile.tsx
@@ -0,0 +1,45 @@
+import {
+  SandpackCodeEditor,
+  SandpackLayout,
+  SandpackProvider,
+  SandpackTests,
+} from "@codesandbox/sandpack-react";
+import React from "react";
+import { useColorMode } from "@docusaurus/theme-common";
+import { githubLight, dracula } from "@codesandbox/sandpack-themes";
+
+const setup = `import "mix-n-matchers/all";`;
+
+export const TestFile: React.FC<{ name: string; children: string }> = ({
+  name,
+  children,
+}) => {
+  const { colorMode } = useColorMode();
+  return (
+    <SandpackProvider
+      theme={colorMode === "dark" ? dracula : githubLight}
+      customSetup={{
+        entry: "entry.js",
+        dependencies: { "mix-n-matchers": "^1" },
+      }}
+      files={{
+        "/entry.js": {
+          code: "",
+          hidden: true,
+        },
+        "/extended.test.js": {
+          code: setup,
+          hidden: true,
+        },
+        [`/${name}.test.js`]: children,
+      }}
+    >
+      <SandpackLayout>
+        <SandpackCodeEditor showTabs />
+        <SandpackTests style={{ minHeight: 150 }} />
+      </SandpackLayout>
+    </SandpackProvider>
+  );
+};
+
+export const ts = String.raw;
