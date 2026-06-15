b09ef4b2c52c7a812df362bd54c7c0d40e10f0c3
Ben Durrant <ben.durrant@marketdojo.com>
2026-06-16 00:56:56 +0100

    create every and some utility functions for asserting on iterables

--- jest.config.ts
+++ jest.config.ts
@@ -1,13 +1,13 @@
 import type { Config } from "jest";
 import { pathsToModuleNameMapper } from "ts-jest";
-import { compilerOptions } from "./tsconfig.json";
+import tsconfig from "./tsconfig.json" with { type: "json" };
 
 const config: Config = {
   preset: "ts-jest/presets/js-with-ts-esm",
   testEnvironment: "node",
   extensionsToTreatAsEsm: [".ts"],
   roots: ["<rootDir>"],
-  moduleNameMapper: pathsToModuleNameMapper(compilerOptions.paths, {
+  moduleNameMapper: pathsToModuleNameMapper(tsconfig.compilerOptions.paths, {
     useESM: true,
     prefix: "<rootDir>/",
   }),
