1b51baf6b0a11141405724cfba50ac8d3bbbedb9
Ben Durrant <ben.durrant@marketdojo.com>
2026-07-09 16:43:37 +0100

    move alias inside test configuration in vitest-dist.config.ts

--- vitest-dist.config.ts
+++ vitest-dist.config.ts
@@ -4,8 +4,8 @@ import defaultConfig from "./vitest.config";
 export default mergeConfig(defaultConfig, {
   test: {
     setupFiles: ["mix-n-matchers/vitest", "./vitest-setup.ts"],
-  },
-  alias: {
-    "mix-n-matchers": "./dist/index.mjs",
+    alias: {
+      "mix-n-matchers": "./dist/index.mjs",
+    },
   },
 });
