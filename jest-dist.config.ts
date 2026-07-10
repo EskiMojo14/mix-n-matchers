5246e8240ff8ff4e947b8381f985778ee7bd2997
Ben Durrant <ben.durrant@marketdojo.com>
2026-07-10 10:53:57 +0100

    remove default options from jest config, and use mutative to create a new config for dist tests

--- jest-dist.config.ts
+++ jest-dist.config.ts
@@ -1,16 +1,11 @@
-import type { Config } from "jest";
 import baseConfig from "./jest.config.ts";
+import { create } from "mutative";
 
-const config = {
-  ...baseConfig,
-  moduleNameMapper: Object.fromEntries(
-    Object.entries(baseConfig.moduleNameMapper ?? {}).filter(
-      ([, value]) => !(typeof value === "string" && value.includes("/src/index")),
-    ),
-  ),
-  setupFilesAfterEnv: ["mix-n-matchers/jest-globals", "<rootDir>/jest-setup.ts"],
-} satisfies Config;
-
-console.log(baseConfig.moduleNameMapper, config.moduleNameMapper);
-
-export default config;
+export default create(baseConfig, (draftConfig) => {
+  draftConfig.setupFilesAfterEnv = ["mix-n-matchers/jest-globals", "<rootDir>/jest-setup.ts"];
+  for (const [key, value] of Object.entries(baseConfig.moduleNameMapper)) {
+    if (typeof value === "string" && value.includes("/src/index")) {
+      delete draftConfig.moduleNameMapper[key];
+    }
+  }
+});
--- jest.config.ts
+++ jest.config.ts
@@ -4,13 +4,11 @@ import tsconfig from "./tsconfig.json" with { type: "json" };
 
 const config = {
   preset: "ts-jest/presets/js-with-ts-esm",
-  testEnvironment: "node",
-  extensionsToTreatAsEsm: [".ts"],
-  roots: ["<rootDir>"],
-  moduleNameMapper: pathsToModuleNameMapper(tsconfig.compilerOptions.paths, {
-    useESM: true,
-    prefix: "<rootDir>/",
-  }),
+  moduleNameMapper:
+    pathsToModuleNameMapper(tsconfig.compilerOptions.paths, {
+      useESM: true,
+      prefix: "<rootDir>/",
+    }) ?? {},
   setupFilesAfterEnv: ["<rootDir>/src/jest-globals.ts", "<rootDir>/jest-setup.ts"],
 } satisfies Config;
 
